6070830adcceb91a2698ba08928114448688eb5f
Showman_Das <Showmandas3216@gmail.com>
2023-05-23 12:13:52 +0600

    section header and sub heading dynamic

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,10 +8,13 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
     return (
-        <Swiper
+        <section>
+        <SectionTitle subHeading={'From 11.00am to 10.00pm'} heading={'order online'}/>
+            <Swiper
         slidesPerView={4}
         spaceBetween={30}
         centeredSlides={true}
@@ -28,6 +31,7 @@ const Category = () => {
         <SwiperSlide><img src={slide5} alt="slide img" /><h3 className="text-4xl text-white uppercase text-center -mt-16"></h3></SwiperSlide>
       
       </Swiper>
+        </section>
     );
 };
 
